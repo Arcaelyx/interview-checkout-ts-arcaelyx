99c51826e760e410a0258d4a018b0401806f70d9
Arcaelyx <arcaelyx@protonmail.com>
2022-07-12 19:51:32 -0400

    fix error where cart items were not loading in header
    This removes the fetching of cart items outside of the cart component
    and into the header component so as to not render zero items whenever
    viewing the catalog page.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,37 +1,31 @@
-import { useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import { formatCentsToDollars } from '../utils/transformers';
-import { handleEffect } from '../handlers'
 
-import { createCart, deleteFromCart, getCart } from '../actions/cart';
+import { createCart, deleteFromCart } from '../actions/cart';
 
 import CartItem from './CartItem';
 
-const Cart = props => {
-  useEffect(handleEffect(props), [])
-  return (
-    <div className='App-product-catalog'>
-      { props.cart.items.map(item => <CartItem key={item.id} {...item} />) }
-      <hr />
-      <div className='App-row'>
-        <div className='App-righter'>
-          <div className='App-flex'>
-            <div className='App-flex-col'>
-              Subtotal:
-            </div>
-            <div className='App-flex-col'>
-              { formatCentsToDollars(props.cart.items.map(item => item['quantity'] * item['priceCents']).reduce((a, b) => a + b, 0)) }
-            </div>
+const Cart = props => (
+  <div className='App-product-catalog'>
+    { props.cart.items.map(item => <CartItem key={item.id} {...item} />) }
+    <hr />
+    <div className='App-row'>
+      <div className='App-righter'>
+        <div className='App-flex'>
+          <div className='App-flex-col'>
+            Subtotal:
+          </div>
+          <div className='App-flex-col'>
+            { formatCentsToDollars(props.cart.items.map(item => item['quantity'] * item['priceCents']).reduce((a, b) => a + b, 0)) }
           </div>
         </div>
       </div>
     </div>
-  );
-}
+  </div>
+);
 
 export default connect(({ cart }) => ({ cart }), {
   createCart,
-  deleteFromCart,
-  getCart
+  deleteFromCart
 })(Cart);
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,31 @@
+import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { formatCentsToDollars, itemQuantity } from '../utils/transformers';
 
+import { getCart } from '../actions/cart';
+
 import CartIcon from './CartIcon';
 
-const Header = props => (
-  <header className='App-header'>
-    <ul className='App-nav'>
-      <li><Link to='/'>Product catalog</Link></li>
-      <li>
-        <Link to='/cart'>
-          <CartIcon />
-          { ' ' }
-          { itemQuantity(props) } in cart | Total:
-          { ' ' }
-          { formatCentsToDollars(props.cart.items.map(item => item['quantity'] * item['priceCents']).reduce((a, b) => a + b, 0)) }
-        </Link>
-      </li>
-    </ul>
-  </header>
-)
+const Header = props => {
+  useEffect(() => props.getCart(props.cart.id), []);
+  return (
+    <header className='App-header'>
+      <ul className='App-nav'>
+        <li><Link to='/'>Product catalog</Link></li>
+        <li>
+          <Link to='/cart'>
+            <CartIcon />
+            { ' ' }
+            { itemQuantity(props) } in cart | Total:
+            { ' ' }
+            { formatCentsToDollars(props.cart.items.map(item => item['quantity'] * item['priceCents']).reduce((a, b) => a + b, 0)) }
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+}
 
-export default connect(({ cart }) => ({ cart }))(Header);
+export default connect(({ cart }) => ({ cart }), { getCart })(Header);
