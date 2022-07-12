e07262a45098b4e869f775d26007735269ce3b51
Arcaelyx <arcaelyx@protonmail.com>
2022-07-12 19:52:47 -0400

    ensure that application does not crash
    This modifies the basic test that ensures that the application does not
    catastrophically crash upon a basic page load by adding the redux
    provider to the unit test.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { render, screen } from '@testing-library/react';
+
+import store from './reducers';
+
 import App from './App';
 
 test('renders without crashing', () => {
-  render(<App />);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 });
